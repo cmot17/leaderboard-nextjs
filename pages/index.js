b32bcda2405ec3a5f75a16b011583ac3f5309171
cmot17 <25753364+cmot17@users.noreply.github.com>
2021-12-05 19:18:02 -0500

    remove unused and broken css import

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
 export default function Home() {
-  return <p>Hello World</p>;
+    return <p>Hello World</p>;
 }
